77e3501e28501e0e8255e8ef6c20c989b5550be3
unknown <Adriano@adriano.fastcompras.fastlocal.com>
2015-07-02 11:44:45 -0300

    Database

---
+++ DeskDoc/database/deskdrive.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE groups (
+idGroup serial primary key,
+groupName varchar(10)
+);
+
+CREATE TABLE users (
+idUser serial primary key,
+username varchar(10),
+password varchar(10),
+idGroupFK int,
+FOREIGN KEY (idGroupFK) REFERENCES groups(idGroup) 
+);
+
+
+insert into groups values (1, 'admin');
+insert into groups values (2, 'user');
+
+
+insert into users values (1, 'teste', 'pass', 1);
